7ec954c97985237d57ce640fd687139396488f18
RanjithKumar <ranjithkumar.sv@infovision.com>
2022-04-11 13:02:59 +0530

    iniital

---
+++ precache-manifest.83a0cb33fe82259fdd3205725e45a768.js
@@ -0,0 +1,158 @@
+self.__precacheManifest = [
+  {
+    "revision": "620a9dc08ba3b2f88b82",
+    "url": "./static/css/main.0c886af4.chunk.css"
+  },
+  {
+    "revision": "620a9dc08ba3b2f88b82",
+    "url": "./static/js/main.620a9dc0.chunk.js"
+  },
+  {
+    "revision": "4ca6072524205c472c84",
+    "url": "./static/js/1.4ca60725.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "394a767696b47f60e7c3f9c76043a301",
+    "url": "./static/js/zcv.wasm"
+  },
+  {
+    "revision": "62307b3c39d9cdc940d46fc488e225d0",
+    "url": "./static/js/worker.24b1ca36.worker.js"
+  },
+  {
+    "revision": "ef9441c40dee8450e168d330e2349474",
+    "url": "./static/media/unknown-person.ef9441c4.png"
+  },
+  {
+    "revision": "3c42e994caf417ce533c4f07957e5d62",
+    "url": "./static/media/home-star-unfill.3c42e994.png"
+  },
+  {
+    "revision": "d58bde9eb5c928b0e471a79c687d2368",
+    "url": "./b4f46e148a3b3b5aef90717d8d60e3fc.zbin"
+  },
+  {
+    "revision": "2a75156f883d7da67337aef086e2831e",
+    "url": "./static/media/mic_on.2a75156f.png"
+  },
+  {
+    "revision": "8300f947210b0f13fa94f33a32539318",
+    "url": "./static/media/mic_off.8300f947.png"
+  },
+  {
+    "revision": "8b1769443d29161093738d17a1d599ce",
+    "url": "./static/media/end-call.8b176944.png"
+  },
+  {
+    "revision": "5e7309bd96a9334d07f42f518beba87b",
+    "url": "./static/media/faceMeshTemplate.5e7309bd.png"
+  },
+  {
+    "revision": "bc543ca186b9af3903c71b5baf707f20",
+    "url": "./static/media/VERIZONNHGDS-BOLD.bc543ca1.TTF"
+  },
+  {
+    "revision": "2db9b74ace1a0061061dc38cc1d4318e",
+    "url": "./static/media/VERIZONNHGDS-REGULAR.2db9b74a.TTF"
+  },
+  {
+    "revision": "b5dd7030726727be69f393b3d30f753a",
+    "url": "./static/media/VERIZONNHGDS-ITALIC.b5dd7030.TTF"
+  },
+  {
+    "revision": "69d3cd6ae40f1136efd506db2c3ba62b",
+    "url": "./static/media/VERIZONNHGDS-BOLDITALIC.69d3cd6a.TTF"
+  },
+  {
+    "revision": "09c946f817d2ddacc28410563011ad5c",
+    "url": "./static/media/user.09c946f8.png"
+  },
+  {
+    "revision": "24e69a410868ee416731765426de043a",
+    "url": "./static/media/banner-1.24e69a41.png"
+  },
+  {
+    "revision": "9862265381756c17774858832fcb74c8",
+    "url": "./static/media/banner-2.98622653.png"
+  },
+  {
+    "revision": "c87a41a4ab04c29c8d4d63b927816a52",
+    "url": "./static/media/fav-unfilled.c87a41a4.svg"
+  },
+  {
+    "revision": "7fd159f6133030c9a2b700fbb73a68e1",
+    "url": "./static/media/googleMap.7fd159f6.png"
+  },
+  {
+    "revision": "e82aa94b3317f69dbbc33ced1ad50059",
+    "url": "./f7b62a3a53b5c9b3222ba8dc53cb8b11.zbin"
+  },
+  {
+    "revision": "d092cdebd443de078d6a06730403b404",
+    "url": "./482727c0e7dd40d73f5aae47f238ad61.zbin"
+  },
+  {
+    "revision": "38a995a8d8a850a424a69dceb01f8113",
+    "url": "./static/media/edit_icon.38a995a8.png"
+  },
+  {
+    "revision": "a16fa1a14a7e0265780b26730220b357",
+    "url": "./static/media/appleWatch.a16fa1a1.png"
+  },
+  {
+    "revision": "ce7eb686419b8b7047a11f0c39606898",
+    "url": "./static/media/iPhone13Pro_Blue.ce7eb686.png"
+  },
+  {
+    "revision": "aac1d31174f9a069eecc2a6b40415234",
+    "url": "./static/media/iPhone13Pro_Gold.aac1d311.png"
+  },
+  {
+    "revision": "8ad67f9fa0aa0ed97c7d49129331129b",
+    "url": "./static/media/iPhone13Pro_Graphite.8ad67f9f.png"
+  },
+  {
+    "revision": "5cdfb6b2ac4613136aff2c304afb1f60",
+    "url": "./static/media/Zflip3_Black.5cdfb6b2.png"
+  },
+  {
+    "revision": "cb6909608ba64864568d10a9d8b18b2f",
+    "url": "./static/media/Zflip3_Cream.cb690960.png"
+  },
+  {
+    "revision": "f2a42bfc4f4fa31c898a9bb5bc442c91",
+    "url": "./static/media/Zflip3_Green.f2a42bfc.png"
+  },
+  {
+    "revision": "9a6be783e37da649ba321b615f110d2d",
+    "url": "./static/media/Zflip3_Lavender.9a6be783.png"
+  },
+  {
+    "revision": "d9523dca429ddd12896f0d84d4030749",
+    "url": "./static/media/pixel6Pro_StormyBlack.d9523dca.png"
+  },
+  {
+    "revision": "03fedb5179d6d9986a1f7eb7a03d5621",
+    "url": "./static/media/pixel6Pro_CloudyWhite.03fedb51.png"
+  },
+  {
+    "revision": "f0db21bec04718b211e33b307dfc67ac",
+    "url": "./static/media/pixel6Pro_SortaSunny.f0db21be.png"
+  },
+  {
+    "revision": "9665f87e56a8f0d84268efd8df6bef49",
+    "url": "./static/media/video_on.9665f87e.png"
+  },
+  {
+    "revision": "7c2315874ba78daffa8d513851ebcfda",
+    "url": "./static/media/video_off.7c231587.png"
+  },
+  {
+    "revision": "0e8452e73ac19003d5dd661e92c14d1d",
+    "url": "./index.html"
+  }
+];
